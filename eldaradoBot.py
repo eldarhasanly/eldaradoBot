c81720c471260679cd880a7332de622ae49b5824
Eldar Hasanli <151502641+eldarhasanly@users.noreply.github.com>
2024-10-11 09:21:11 +0400

    Add function to fetch a random fact from the database

--- eldaradoBot.py
+++ eldaradoBot.py
@@ -2,4 +2,13 @@ import sqlite3
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
+def get_random_fact() :
+    conn = sqlite3.connect('facts.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT fact FROM facts ORDER BY RANDOM() LIMIT 1")
+    fakt = cursor.fetchone()[0]
+
+    conn.close()
+    return fakt
 

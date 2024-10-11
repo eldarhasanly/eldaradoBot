f40c2bdc3fc3de6690f32c66525f21e639833199
Eldar Hasanli <151502641+eldarhasanly@users.noreply.github.com>
2024-10-11 09:26:18 +0400

    Add function to handle the /start command

--- eldaradoBot.py
+++ eldaradoBot.py
@@ -2,6 +2,7 @@ import sqlite3
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
+# Function to fetch a random fact from the database
 def get_random_fact() :
     conn = sqlite3.connect('facts.db')
     cursor = conn.cursor()
@@ -12,3 +13,8 @@ def get_random_fact() :
     conn.close()
     return fakt
 
+
+# Function to handle the /start command
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) : 
+    welcome_message = "Eldarado bota xoş gəlmisiniz! Hal-hazırda bazada olan 1000-dən artıq faktdan təsadüfi bir fakt öyrənmək üçün /fakt əmrini daxil edin. © 2024. Eldar Həsənli"
+    await update.message.reply_text(welcome_message)
